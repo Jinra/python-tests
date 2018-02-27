6b735153bb868b64da090ed2c2f6ef8848b4a5c5
Jinra <36873193+Jinra@users.noreply.github.com>
2018-02-26 20:26:30 -0800

    Update New Convert Value Test using Return.py

--- New Convert Value Test using Return.py
+++ New Convert Value Test using Return.py
@@ -1,45 +1,45 @@
+"The purpose of this code is to convert speed units"
 # Take value, seperate values and convert
 
 import string
 
-mph_to_kph = 1.609344
-kph_to_mph = 0.621371192
-
-no_numb = str.maketrans(dict.fromkeys('0123456789'))
-no_lett = str.maketrans(dict.fromkeys(string.ascii_lowercase))
-
-print("The purpose of this code is to convert speed units")
+MPH_TO_KPH = 1.609344
+KPH_TO_MPH = 0.621371192
 
 
-def format_data():  # This will extract the value and unit from the input
-    value = getvalue()
-    inputunit = str(value.translate(no_numb))
-    inputspeed = str(value.translate(no_lett))
-    return inputunit, inputspeed
+def format_data(value):  # This will extract the value and unit from the input
+    no_numb = str.maketrans(dict.fromkeys('0123456789'))
+    no_lett = str.maketrans(dict.fromkeys(string.ascii_lowercase + string.punctuation))
+    input_unit = value.translate(no_numb).split()[0]
+    input_speed = float(value.translate(no_lett))
+    return input_unit, input_speed
 
 
-def speed_converter():  # This will convert speed values interchangeably from mph to kph and vice versa
-    inputunit, inputspeed = format_data()
-    desiredunit = getvalue()
-    if inputunit == "kph" and desiredunit == "mph":
-        print("Value: " + inputspeed + ", " + "oUnit: " + inputunit + ", " + "dUnit: " + desiredunit)
-        print(str(int(float(inputspeed) * kph_to_mph)) + "mph")
-    elif inputunit == "mph" and desiredunit == "kph":
-        print("Value: " + inputspeed + ", " + "oUnit: " + inputunit + ", " + "dUnit: " + desiredunit)
-        print(str(int(float(inputspeed) * mph_to_kph)) + "kph")
-    else:
-        print("Please input your values correctly")
+def speed_converter(unit, speed):  # This will convert speed values interchangeably from mph to kph and vice versa
+    if 'k' in unit and ('m' in unit or 'p' in unit):
+        output_unit = 'mph'
+        print("Value: " + speed + ", " + "oUnit: " + unit + ", " + "dUnit: " + output_unit)
+        print(str(speed * KPH_TO_MPH) + "mph")
+    elif 'm' in unit and ('i' in unit or 'p' in unit):
+        output_unit = 'kph'
+        print("Value: " + speed + ", " + "oUnit: " + unit + ", " + "dUnit: " + output_unit)
+        print(str(speed * MPH_TO_KPH) + "kph")
 
 
-def getvalue():  # This will get values
+def get_value():  # This will get values
     while True:
         try:
-            value, to_, desiredunit = input('Input, e.g: 200mph to kph ').split()
-            format_data()
-            speed_converter()
-            break
+            input = input('Input, e.g: 200mph to kph ').lower()
+            return input
         except ValueError:
-            value, to_, desiredunit = input('Not valid data, try again! ').split()
-    return value, desiredunit
+            print('Not valid data, try again!')
 
-getvalue()
+
+def main():
+    input = get_value()
+    unit, speed = format_data(input)
+    speed_converter(unit, speed)
+    
+
+if __name__ == '__main__':
+    getvalue()
